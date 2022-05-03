d4d241d97ec4a7019663ea6dc39e58103e366022
KAHO <84381486+michimichi2021@users.noreply.github.com>
2022-05-03 17:46:51 +0900

    完成

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,10 @@ export const App = () => {
 
   return (
     <>
-      <InputTodo todoText={todoText} onChange={onChangeTodoText} onClick={onClickAdd}/>
+      <InputTodo todoText={todoText} onChange={onChangeTodoText} onClick={onClickAdd} disabled={incompleteTodos.length >= 5}/>
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: 'red'}}>登録できるtodo5個までだよ〜。消化しろ〜。</p>
+      )}
       <IncompleteTodos todos={incompleteTodos} onClickComplete={onClickComplete} onClickDelete={onClickDelete}/>
       <CompleteTodos todos={completeTodos} onClickBack={onClickBack}/>
     </>
